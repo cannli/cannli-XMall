b38c09fd5045a398d537e285d86c191cda90844b
cannli <405533738@qq.com>
2020-08-21 21:04:49 +0800

    1

---
+++ fin-plat/src/views/login/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <div class="wrapper">
+      <div style="text-align: center;">
+        <img :src="smartisan_4" width="100px"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import smartisan_4 from '@/assets/img/global/smartisan_4ada7fecea.png'
+
+  export default {
+    name: "index",
+    data() {
+      return {
+        smartisan_4,
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" type="text/scss" rel="stylesheet/scss">
+  @import 'assets/css/base/basicStyle.scss';
+
+  .login {
+    @include Absolute();
+
+    .wrapper {
+      width: 450px;
+    }
+  }
+</style>
